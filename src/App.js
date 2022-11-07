91e58b1c28cf8dd30d9f71437ca14b9ace6e86b1
Ngo Van Quan <vq.2509.2003@gmail.com>
2022-11-07 17:17:13 +0700

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import 'antd/dist/antd.css';
+import { Routes, Route, unstable_HistoryRouter as HistoryRouter } from 'react-router-dom';
+import { history } from './utils/history';
+import LayoutAdmin from './templates/admins/LayoutAdmin';
+import Layout from './templates/users/Layout';
+import TrangChu from './pages/TrangChu/TrangChu';
+import TimKiem from './pages/TimKiem/TimKiem';
+import NotFound from './pages/NotFound';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HistoryRouter history={history} >
+        <Routes>
+          {/* user */}
+          <Route path='/' element={<Layout />}>
+            <Route index path='/' element={<TrangChu />} />
+            <Route path='/timkiem' element={<TimKiem />} />
+
+            <Route path='*' element={<NotFound />} />
+          </Route>
+
+          {/* admin */}
+          <Route path='/admin' element={<LayoutAdmin />}>
+
+          </Route>
+        </Routes>
+      </HistoryRouter>
     </div>
   );
 }
